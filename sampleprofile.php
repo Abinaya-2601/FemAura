ed3d006d928ad4bc143b9607550d0a34cbeae96a
Abinaya-2601 <sec22ci024@sairamtap.edu.in>
2025-08-17 14:05:53 +0530

    Add files via upload

---
+++ sampleprofile.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FemAura Home</title>
+
+  <!-- FAVICON -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+  <!-- Custom File's Link -->
+  <link rel="stylesheet" href="CSS/style.css">
+  <!-- <link rel="stylesheet" href="responsive-style.css"> -->
+
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="100">
+
+  <!-- Navbar section -->
+  <header class="header_wrapper">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img href="home.php" src="images/icon1.png" class="img-fluid" style="width:150px; height:150px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <!-- <span class="navbar-toggler-icon"></span> -->
+          <i class="fas fa-stream navbar-toggler-icon"></i>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav  menu-navbar-nav" style="gap:3.5rem">
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="FindGroup.php">Event</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Blog.php">Security</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="sampleprofile.php">Profile</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
